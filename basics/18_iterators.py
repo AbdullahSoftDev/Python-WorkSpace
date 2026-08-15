9d60ce99ef9cd72ace82a5dfef70ee45080529b1
AbdullahSoftDev <m.abdullah.cs23@gmail.com>
2026-08-15 11:00:31 +0500

    Add basics/18_iterators.py: Custom iterators

---
+++ basics/18_iterators.py
@@ -0,0 +1,16 @@
+# Custom iterator
+class CountDown:
+    def __init__(self, start):
+        self.current = start
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current <= 0:
+            raise StopIteration
+        self.current -= 1
+        return self.current + 1
+
+for num in CountDown(5):
+    print(num)
