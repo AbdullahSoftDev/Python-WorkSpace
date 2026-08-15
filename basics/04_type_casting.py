d790dcf48fcd2f990b8fd3025b005c06dec807e3
AbdullahSoftDev <m.abdullah.cs23@gmail.com>
2026-08-15 11:00:08 +0500

    Add basics/04_type_casting.py: Type conversion examples

---
+++ basics/04_type_casting.py
@@ -0,0 +1,19 @@
+# int to float
+x = 10
+y = float(x)
+
+# float to int
+a = 3.14
+b = int(a)
+
+# string to int
+s = "123"
+n = int(s)
+
+# list to tuple
+lst = [1, 2, 3]
+tup = tuple(lst)
+
+# tuple to list
+t = (1, 2, 3)
+l = list(t)
